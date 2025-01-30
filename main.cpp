6e74bd12ab9a8c4e63072eeea3cf1a1cb6a6ed7e
Vaquh <Vaquh@users.noreply.github.com>
2025-01-30 15:39:43 -0500

    refactor main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <numeric>
 #include <random>
+#include <string>
 #include <vector>
 
 template <typename T>
@@ -14,63 +15,100 @@ double getAverage(std::vector<T> const& v)
 
   return std::reduce(v.begin(), v.end(), 0.0) / v.size();
 }
- 
-void simulate(int maxHit, double accuracy, int atkspd, int attempts)
+
+int randDamage(int minHit, int maxHit)
 {
+  std::random_device rd;
+  std::mt19937_64 dmgGenerator(rd());
+  std::uniform_int_distribution<int> dmgDistribution(minHit, maxHit);
+  
+  return dmgDistribution(dmgGenerator);
+}
+
+int main()
+{
+  int maxhit = 33;
+  double accuracy = 0.4191;
+  int atkspd = 4;
+  int attempts = 100000;
+  
+  int attemptsWidth = std::to_string(attempts).length();
+
   std::vector<int> simulation;
 
   std::random_device rd;
-  std::mt19937 accgen(rd());
-  
-  std::uniform_real_distribution<double> accdis(0.0, 1.0);
+  std::mt19937_64 accGenerator(rd());
+  std::uniform_real_distribution<double> accDistribution(0.0, 1.0);
 
   for (size_t i = 0; i < attempts; i++)
   {
     int attack;
 
-    std::random_device rd;
-    std::mt19937 hitgen(rd());
-
-    if(accuracy >= accdis(accgen)) 
+    if(accuracy >= accDistribution(accGenerator)) 
     { // Hit first
-      std::uniform_int_distribution<int> hitdis((int)(maxHit * 0.5), maxHit - 1);
-      int hit = hitdis(hitgen);
-      attack = hit + (int)(hit * 0.5) + (int)(hit * 0.25) + ((int)(hit * 0.25) + 1);
-      std::cout << std::setw(2) << std::left << attack << " : " << std::setw(2) << std::right << hit << " + " << std::setw(2) << std::right << (int)(hit * 0.5) << " + " << std::setw(2) << std::right << (int)(hit * 0.25) << " + " << std::setw(2) << std::right << ((int)(hit * 0.25) + 1) << "\n";
+      int dmg = randDamage(static_cast<int>(maxhit * 0.5), maxhit - 1);
+      attack = dmg + static_cast<int>(dmg * 0.5) + static_cast<int>(dmg * 0.25) + (static_cast<int>(dmg * 0.25) + 1);
+      std::cout << std::setw(attemptsWidth) << std::right << i + 1                            << " : "
+                << std::setw(2)             << std::right << attack                           << " - "
+                << std::setw(2)             << std::right << dmg                              << " + "
+                << std::setw(2)             << std::right << static_cast<int>(dmg * 0.5)      << " + "
+                << std::setw(2)             << std::right << static_cast<int>(dmg * 0.25)     << " + "
+                << std::setw(2)             << std::right << static_cast<int>(dmg * 0.25) + 1 << "\n";
     }
-    else if (accuracy >= accdis(accgen))
+    else if (accuracy >= accDistribution(accGenerator))
     { // Hit second
-      std::uniform_int_distribution<int> hitdis((int)(maxHit * 0.375), (int)(maxHit * 0.875));
-      int hit = hitdis(hitgen);
-      attack = 0 + hit + (int)(hit * 0.5) + ((int)(hit * 0.5) + 1);
-      std::cout << std::setw(2) << std::left << attack << " : " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << hit << " + " << std::setw(2) << std::right << (int)(hit * 0.5) << " + " << std::setw(2) << std::right << ((int)(hit * 0.5) + 1) << "\n";
+      int dmg = randDamage(static_cast<int>(maxhit * 0.375), static_cast<int>(maxhit * 0.875));
+      attack = 0 + dmg + static_cast<int>(dmg * 0.5) + (static_cast<int>(dmg * 0.5) + 1);
+      std::cout << std::setw(attemptsWidth) << std::right << i + 1                           << " : "
+                << std::setw(2)             << std::right << attack                          << " : "
+                << std::setw(2)             << std::right << 0                               << " + "
+                << std::setw(2)             << std::right << dmg                             << " + "
+                << std::setw(2)             << std::right << static_cast<int>(dmg * 0.5)     << " + "
+                << std::setw(2)             << std::right << static_cast<int>(dmg * 0.5) + 1 << "\n";
     }
-    else if (accuracy >= accdis(accgen))
+    else if (accuracy >= accDistribution(accGenerator))
     { // Hit third
-      std::uniform_int_distribution<int> hitdis((int)(maxHit * 0.25), (int)(maxHit * 0.75));
-      int hit = hitdis(hitgen);
-      attack = 0 + 0 + hit + (hit + 1);
-      std::cout << std::setw(2) << std::left << attack << " : " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << hit << " + " << std::setw(2) << std::right << (hit + 1) << "\n";
+      int dmg = randDamage(static_cast<int>(maxhit * 0.25), static_cast<int>(maxhit * 0.75));
+      attack = 0 + 0 + dmg + (dmg + 1);
+      std::cout << std::setw(attemptsWidth) << std::right << i + 1   << " : "
+                << std::setw(2)             << std::right << attack  << " : "
+                << std::setw(2)             << std::right << 0       << " + "
+                << std::setw(2)             << std::right << 0       << " + "
+                << std::setw(2)             << std::right << dmg     << " + "
+                << std::setw(2)             << std::right << dmg + 1 << "\n";
     }
-    else if (accuracy >= accdis(accgen))
+    else if (accuracy >= accDistribution(accGenerator))
     { // Hit fourth
-      std::uniform_int_distribution<int> hitdis((int)(maxHit * 0.25), (int)(maxHit * 1.25));
-      int hit = hitdis(hitgen);
-      attack = 0 + 0 + 0 + hit;
-      std::cout << std::setw(2) << std::left << attack << " : " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << hit << "\n";
+      int dmg = randDamage(static_cast<int>(maxhit * 0.25), static_cast<int>(maxhit * 1.25));
+      attack = 0 + 0 + 0 + dmg;
+      std::cout << std::setw(attemptsWidth) << std::right << i + 1  << " : "
+                << std::setw(2)             << std::right << attack << " : "
+                << std::setw(2)             << std::right << 0      << " + "
+                << std::setw(2)             << std::right << 0      << " + "
+                << std::setw(2)             << std::right << 0      << " + "
+                << std::setw(2)             << std::right << dmg    << "\n";
     }
     else
     { // Miss
-      std::uniform_int_distribution<int> hitdis(0, 2);
-      if(hitdis(hitgen) < 2)
+      if(randDamage(0, 2) < 2)
       {
         attack = 0 + 0 + 1 + 1;
-        std::cout << std::setw(2) << std::left << attack << " : " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 1 << " + " << std::setw(2) << std::right << 1 << "\n";
+        std::cout << std::setw(attemptsWidth) << std::right << i + 1  << " : "
+                  << std::setw(2)             << std::right << attack << " : "
+                  << std::setw(2)             << std::right << 0      << " + "
+                  << std::setw(2)             << std::right << 0      << " + "
+                  << std::setw(2)             << std::right << 1      << " + "
+                  << std::setw(2)             << std::right << 1      << "\n";
       }
       else
       {
         attack = 0 + 0 + 0 + 0;
-        std::cout << std::setw(2) << std::left << attack << " : " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << " + " << std::setw(2) << std::right << 0 << "\n";
+        std::cout << std::setw(attemptsWidth) << std::right << i + 1  << " : "
+                  << std::setw(2)             << std::right << attack << " : "
+                  << std::setw(2)             << std::right << 0      << " + "
+                  << std::setw(2)             << std::right << 0      << " + "
+                  << std::setw(2)             << std::right << 0      << " + "
+                  << std::setw(2)             << std::right << 0      << "\n";
       }
     }
 
@@ -80,22 +118,10 @@ void simulate(int maxHit, double accuracy, int atkspd, int attempts)
   double damageAverage = getAverage(simulation);
   double damagePerSecond = (damageAverage / atkspd) / 0.6;
 
-  std::cout << "\n";
-  std::cout << "Simulated " << simulation.size() << " attacks\n";
-  std::cout << "Average Hit: " << damageAverage << "\n";
-  std::cout << "Dps: " << damagePerSecond << "\n";
-
-  return;
-}
-
-int main()
-{
-  int maxhit = 33;
-  double accuracy = 0.4191;
-  int atkspd = 4;
-  int attempts = 100000;
-
-  simulate(maxhit, accuracy, atkspd, attempts);
+  std::cout << "\n"
+            << "Simulated "    << simulation.size() << " attacks\n"
+            << "Average Hit: " << damageAverage     << "\n"
+            << "Dps: "         << damagePerSecond   << "\n";
 
   return 0;
 }
